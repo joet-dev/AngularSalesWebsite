19756083ca035031e134afe4fc13d4ca2e87e8d4
joet-dev <joseph.thurlow7d0@gmail.com>
2023-06-01 01:52:25 +1000

    Fully responsive site with sale items arranged in a grid.

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -7,18 +7,60 @@ import { SaleItem } from '../saleitem';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, 
-            SaleItemComponent],
+  imports: [CommonModule,
+    SaleItemComponent],
   templateUrl: `./home.component.html`,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  saleItem : SaleItem = {
-    id: 1, 
-    title: 'Fast Car',
-    price: 100, 
-    modelNo: "XYZ123", 
-    photo: 'assets/logo.svg',
-    description: "Fast matchbox car." 
-  };
+  saleItemList: SaleItem[] = [
+    {
+      id: 1,
+      title: 'Fast Car',
+      price: 100,
+      modelNo: "XYZ123",
+      photo: 'assets/logo.svg',
+      description: "Fast matchbox car."
+    },
+    {
+      id: 2,
+      title: 'Example 1',
+      price: 100000,
+      modelNo: "TEST123",
+      photo: 'assets/logo.svg',
+      description: "Fast matchbox car."
+    },
+    {
+      id: 3,
+      title: 'Example 2',
+      price: 100000,
+      modelNo: "TEST123",
+      photo: 'assets/logo.svg',
+      description: "Fast matchbox car."
+    },
+    {
+      id: 4,
+      title: 'Example 3',
+      price: 100000,
+      modelNo: "TEST123",
+      photo: 'assets/logo.svg',
+      description: "Fast matchbox car."
+    },
+    {
+      id: 5,
+      title: 'Example 4',
+      price: 100000,
+      modelNo: "TEST123",
+      photo: 'assets/logo.svg',
+      description: "Fast matchbox car."
+    },
+    {
+      id: 6,
+      title: 'Example 5',
+      price: 100000,
+      modelNo: "TEST123",
+      photo: 'assets/logo.svg',
+      description: "Fast matchbox car."
+    },
+  ];
 }
